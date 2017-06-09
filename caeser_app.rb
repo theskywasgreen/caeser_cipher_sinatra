48539d22d94e140a2704cfac3af4e7aafffeebb6
Nicky Heise <nicky.heise@gmail.com>
2017-06-09 15:55:53 +0100

    add Gemfile and config.ru

--- caeser_app.rb
+++ caeser_app.rb
@@ -9,22 +9,36 @@ LAST_LOWER = "z".ord
 get '/' do
   string = params['string']
   shift_factor = params['shift'].to_i
+  encoded_message = encode(string, shift_factor)
+#  bg_color = css_color(string)
+  erb :index, :locals => {:encoded_message => encoded_message, :string => string}
 end
 
-def caeser_cipher(string, shift)
-  cipher = ""
+def encode(string, shift_factor)
+  return 'You cannot encode nothing' if string.nil? || string == ""
+  return string if shift_factor.zero?
+  encoded_message = encode_chars(string, shift_factor)
+  encoded_message
+end
 
-  string.split("").each do |char|
+def encode_chars(string, shift_factor)
+  encoded_message = ''
+  string.each_char do |char|
     if char.ord.between?(FIRST_UPPER, LAST_UPPER)
-      char = ((((char.ord - FIRST_UPPER) + shift) % 26) + FIRST_UPPER).chr
+      char = ((((char.ord - FIRST_UPPER) + shift_factor) % 26) + FIRST_UPPER).chr
     elsif char.ord.between?(FIRST_LOWER, LAST_LOWER)
-      char = ((((char.ord - FIRST_LOWER) + shift) % 26) + FIRST_LOWER).chr
+      char = ((((char.ord - FIRST_LOWER) + shift_factor) % 26) + FIRST_LOWER).chr
     end
-
-  cipher << char
-
+    encoded_message << char
   end
-
-  return cipher
-
+  encoded_message
 end
+
+
+
+#def css_color(string)
+#  case
+#  when string.to_s.length == 4 then "#ffd5be"
+#  when string.to_s.length == 5 then "#ff0064"
+#  end
+#end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './caeser_app'
+run Sinatra::Application
